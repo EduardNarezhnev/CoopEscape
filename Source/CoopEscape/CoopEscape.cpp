b26b9360ce7c13f1506a1db1159c9578a7a552fa
Eduard Narezhnev <narezhnev.ed@gmail.com>
2026-09-21 23:25:36 +0300

    Init project structure with .gitignore

---
+++ Source/CoopEscape/CoopEscape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoopEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoopEscape, "CoopEscape" );
